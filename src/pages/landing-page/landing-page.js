10a41855d530878e1265884cddb9e421c5098782
Evans Ibok <evansibok@gmail.com>
2020-04-12 12:48:41 +0100

    chore: add landing page navigation

--- src/pages/landing-page/landing-page.js
+++ src/pages/landing-page/landing-page.js
@@ -1,20 +1,23 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 
+import brandImage from '../../assets/thankful-logo.png';
 import "./landing-page.styles.scss";
 
 function LandingPage() {
 
   return (
     <div className="landing-page-con">
-      <header className="">
-        <div>
-          <img className="brand-logo" alt="brand-logo" />
+      <header>
+        <div className="brand-image">
+          <Link to="/">
+            <img src={brandImage} className="brand-logo" alt="brand-logo" />
+          </Link>
         </div>
 
         <nav>
           <NavLink to="/login">Log In</NavLink>
-          <NavLink to="/sign-up">Create Account</NavLink>
+          <NavLink to="/register">Create Account</NavLink>
         </nav>
       </header>
       <main>
